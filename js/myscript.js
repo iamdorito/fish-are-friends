7ee5f7b62a7a54fa15cdf231317635eba9fc72ef
Dorothy <dorothychauu@gmail.com>
2015-05-15 15:54:35 -0400

    fish are friends
    an informative site

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+var submit = function () {
+    var x = $('#email').val();
+    
+    if (x == null || x == "") {
+        alert("Email must be valid.");
+        return false;
+    }
+    else {
+        alert("Thanks so much for supporting Fish are Friends! We can't wait to help the Earth get better together");
+    }
+}
